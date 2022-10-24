6d154ca247bc67a3e34553b4998f65525fb6c911
Urban <urban.kocnik@gmail.com>
2022-10-24 18:09:08 +0200

    setting up

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <div className="App">
+      
+      <BrowserRouter>
+        <Routes>
+            
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
